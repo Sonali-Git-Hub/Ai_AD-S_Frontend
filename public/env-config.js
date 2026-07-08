c0d7175c14c482e798529c7fc201f37a3f3907ff
Sonali-Git-Hub <Sonaligupta190322@gmail.com>
2026-07-08 17:22:08 +0530

    Just commit

--- public/env-config.js
+++ public/env-config.js
@@ -1,3 +1,4 @@
 window._env_ = {
-    VITE_AISA_BACKEND_API: "https://aisa24.com/api"
+    VITE_AISA_BACKEND_API: "http://localhost:8080/api"
+    // VITE_AISA_BACKEND_API: "https://aisa24.com/api"
 };
